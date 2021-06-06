b684d537b533b61e40216ebc35989bad563bc71f
Pratik Chaudhari <pratikc1020@gmail.com>
2021-06-06 10:29:35 +0530

    Added feature to display win margin

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,24 +1,39 @@
 #!/bin/bash -x
 
-Heads=0
-Tails=0
-limit=10
+Heads=0;
+Tails=0;
+limit=20;	# limit = (Required limit - 1)
 
-while [ $Heads -le $limit ] && [ $Tails -le $limit ]
+while [ $Heads -le $limit ] && [ $Tails -le $limit ];	# Either Heads or Tails reach limit, while loop will end.
 do
-        random=$(( RANDOM%2 ))
+        random=$(( RANDOM%2 ));		#Generating random number.
 
-        if [ $random -eq 0 ]
+        if [ $random -eq 0 ];
         then
-                Heads=$(( $Heads + 1 ))
+                Heads=$(( $Heads + 1 ));	#Assigning Heads.
 
         else
-                Tails=$(( $Tails + 1 ))
+                Tails=$(( $Tails + 1 ));	#Assigning Tails.
         fi
 done
 
-echo "Welcome to flip coin simulator"
+echo "Welcome to flip coin simulator";
 sleep 1
 
-echo "Number of Heads : " $Heads
-echo "Number of Tails : " $Tails
+echo "Number of Heads : " $Heads;
+echo "Number of Tails : " $Tails;
+
+
+if [ $Heads -gt $Tails ];
+then
+	WinBy=$(( $Heads - $Tails ));
+	echo "Heads win by margin of $WinBy !";
+
+elif [ $Heads -lt $Tails ];
+then
+	WinBy=$(( $Tails - $Heads ));
+	echo "Tails win by margin of $WinBy !";
+elif [ $Heads -eq $Tails ];
+then
+	echo "It's a TIE!"
+fi
